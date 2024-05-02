c560b4461cfb8aed385e7c8804b4a25b61f404bd
sanglq5 <sqle157@gmail.com>
2024-05-02 21:06:56 +0700

    Add tailwind prettier plug and setup mongodb connection

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,14 @@
-import Hero from '@/components/Hero';
-import InfoBoxes from '@/components/InfoBoxes';
-import HomeProperties from '@/components/HomeProperties';
+import Hero from "@/components/Hero";
+import InfoBoxes from "@/components/InfoBoxes";
+import HomeProperties from "@/components/HomeProperties";
 
 const HomePage = () => {
-	return (
-		<>
-			<Hero />
-			<InfoBoxes />
-			<HomeProperties />
-		</>
-	);
+  return (
+    <>
+      <Hero />
+      <InfoBoxes />
+      <HomeProperties />
+    </>
+  );
 };
 export default HomePage;

40f2474e25e202ed79d459a169f67d401e15111a
sanglq5 <sqle157@gmail.com>
2024-05-02 21:47:43 +0700

    Add GET all properties API route & utilities file for request

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,21 +1,26 @@
-import PropertyCard from '@/components/PropertyCard';
-import properties from '@/properties.json';
+import PropertyCard from "@/components/PropertyCard";
+import { fetchProperties } from "@/utils/request";
 
-const PropertiesPage = () => {
-	return (
-		<section className='px-4 py-6'>
-			<div className='container-xl lg:container m-auto px-4 py-6'>
-				{properties.length === 0 ? (
-					<p>No properties found</p>
-				) : (
-					<div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-						{properties.map((property) => (
-							<PropertyCard key={property._id} property={property} />
-						))}
-					</div>
-				)}
-			</div>
-		</section>
-	);
+const PropertiesPage = async () => {
+  const properties = await fetchProperties();
+
+  // Sort properties by date
+  properties.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+
+  return (
+    <section className="px-4 py-6">
+      <div className="container-xl m-auto px-4 py-6 lg:container">
+        {properties.length === 0 ? (
+          <p>No properties found</p>
+        ) : (
+          <div className="grid grid-cols-1 gap-6 md:grid-cols-3">
+            {properties.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
 };
 export default PropertiesPage;

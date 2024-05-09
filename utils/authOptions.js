fc8299eac2baeb0f886d931b3740061bcce3236f
sanglq5 <sqle157@gmail.com>
2024-05-09 21:56:55 +0700

    Implement authentication actions (sign in, logout) and add middleware

--- utils/authOptions.js
+++ utils/authOptions.js
@@ -1,3 +1,5 @@
+import connectDB from "@/config/database";
+import User from "@/models/User";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
@@ -18,15 +20,30 @@ export const authOptions = {
     // Invoked on successful signin
     async signIn({ profile }) {
       // 1. Connect to your database
+      await connectDB();
       // 2. Check if the user exists in your database
+      const userExists = await User.findOne({ email: profile.email });
       // 3. If not, then add user to your database
+      if (!userExists) {
+        // Truncate user name if too long
+        const username = profile.name.slice(0, 20);
+        await User.create({
+          email: profile.email,
+          username,
+          image: profile.picture,
+        });
+      }
       // 4. Return true to allow signin
+      return true;
     },
     // Modifies the session object
     async session({ session }) {
       // 1. Get user from your database
+      const user = await User.findOne({ email: session.user.email });
       // 2. Assign the user id to the session
+      session.user.id = user._id;
       // 3. Return the session object
+      return session;
     },
   },
 };
